32f185731f68d884bd52ee94cc124939838a9943
yfsanchez <yfsanchez@gmail.com>
2022-06-12 01:08:30 -0300

    Consume tu primera API REST : homework
    Consume tu primera API REST : homework

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ console.log('Hello world !!!');
 
 const API_URL = 'https://api.thecatapi.com/v1/images/search';
 
-fetch(API_URL)
-.then(res => res.json())
-.then(data => {
-    const img = document.querySelector('img')
-    img.src = data[0].url
-})
+
+async function reload(){
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    
+    const img = document.querySelector('img');
+    img.src = data[0].url;
+    
+
+}
+
+reload();
