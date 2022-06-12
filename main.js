e520d7f562921f5d578f6a4637cdb886d22ea10a
yfsanchez <yfsanchez@gmail.com>
2022-06-12 18:30:12 -0300

    GET: leyendo michis favoritos
    GET: leyendo michis favoritos

--- main.js
+++ main.js
@@ -1,22 +1,52 @@
-console.log('Hello world !!!');
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=2b4ff403-2b9c-451a-94c7-8f54171b627e';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=3&api_key=2b4ff403-2b9c-451a-94c7-8f54171b627e';
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=2b4ff403-2b9c-451a-94c7-8f54171b627e';
 
+const spanError = document.getElementById('error')
 
-async function reload(){
-    const res = await fetch(API_URL);
+async function loadRandomCats(){
+    const res = await fetch(API_URL_RANDOM);
     const data = await res.json();
-    
-    const image01 = document.getElementById('image01');
-    const image02 = document.getElementById('image02');
-    const image03 = document.getElementById('image03');
-    
-    image01.src = data[0].url;
-    image02.src = data[1].url;
-    image03.src = data[2].url;
-    //console.log(data);
-    
+    console.log('Random');
 
+    if (res.status != 200) {
+        
+        spanError.innerHTML = "Error: " + res.status;
+
+    } else {
+        
+        const image01 = document.getElementById('image01');
+        const image02 = document.getElementById('image02');
+        const image03 = document.getElementById('image03');
+        
+        image01.src = data[0].url;
+        image02.src = data[1].url;
+        image03.src = data[2].url;
+    }
+    
 }
 
-reload();
+async function loadFavoritesCats(){
+    const res = await fetch(API_URL_FAVORITES);
+    const data = await res.json();
+    console.log('Favorites');
+
+    if (res.status != 200) {
+        
+        spanError.innerHTML = "Error: " + res.status;
+
+    } else {
+        
+        const image01 = document.getElementById('image01');
+        const image02 = document.getElementById('image02');
+        const image03 = document.getElementById('image03');
+        
+        image01.src = data[0].url;
+        image02.src = data[1].url;
+        image03.src = data[2].url;
+    }
+    
+}
+
+loadRandomCats();
+loadFavoritesCats();
