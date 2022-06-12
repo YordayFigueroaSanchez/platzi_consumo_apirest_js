318378496d0d4c0639c0942c4c87d78aa8d65a22
yfsanchez <yfsanchez@gmail.com>
2022-06-12 17:01:59 -0300

    Endpoints y query parameters
    Endpoints y query parameters

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 console.log('Hello world !!!');
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search';
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
 
 
 async function reload(){
     const res = await fetch(API_URL);
     const data = await res.json();
     
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+    const image01 = document.getElementById('image01');
+    const image02 = document.getElementById('image02');
+    const image03 = document.getElementById('image03');
+    
+    image01.src = data[0].url;
+    image02.src = data[1].url;
+    image03.src = data[2].url;
+    //console.log(data);
     
 
 }
